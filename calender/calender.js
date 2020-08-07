1812bac64174ad5464067df86eb574c53b5eab68
Aus <abc2614856@gmail.com>
2020-08-08 02:18:38 +0800

    AAAAAAA

--- calender/calender.js
+++ calender/calender.js
@@ -87,7 +87,7 @@ function createCalender(date) {
         if (firstDay <= i) {
             firstTd.addEventListener('click', function (e) {
                 $('#exampleModal').modal('show');
-                selectedDay = e.target.closest('td');
+                selectedDay = e.target.closest('th');
                 let key = `${nowMonth.getFullYear()}` + " " + `${nowMonth.getMonth()}` + " " + `${selectedDay.innerText.split(/\s+/)[0]}`
                 if (localStorage.hasOwnProperty(key)) {
                     console.log(2)
